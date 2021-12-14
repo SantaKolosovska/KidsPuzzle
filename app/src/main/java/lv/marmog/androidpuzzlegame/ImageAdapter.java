6ca6a950c229055211ff9f0716bd5f2cd9770da5
SantaKolosovska <santa.kolosovska@gmail.com>
2021-12-14 20:42:01 +0200

    PUZ-1 initial code

--- app/src/main/java/lv/marmog/androidpuzzlegame/ImageAdapter.java
+++ app/src/main/java/lv/marmog/androidpuzzlegame/ImageAdapter.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ImageAdapter extends BaseAdapter {
+
     private Context mContext;
     private AssetManager am;
     private String[] files;
